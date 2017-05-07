8d8bfbc85d6cf8a32a64eac89b9ae57512646df8
Tomoatsu Shimada <shimada@walbrix.com>
2017-05-07 13:29:12 +0900

    お問い合わせフォームにXSRF対策を追加

--- src/main/scala/com/walbrix/cms/InquiryServlet.scala
+++ src/main/scala/com/walbrix/cms/InquiryServlet.scala
@@ -5,25 +5,28 @@ class InquiryServlet extends org.scalatra.ScalatraServlet with org.scalatra.json
 
   var appConfig:com.typesafe.config.Config = _
 
+  val XSRF_TOKEN_SESSION_ATTR_NAME = "inquiry-xsrf-token"
+
   override def initialize(config: javax.servlet.ServletConfig): Unit = {
     super.initialize(config)
     this.appConfig = edu.gatech.gtri.typesafeconfigextensions.forwebapps.WebappConfigs.webappConfigFactory(servletContext).load
   }
 
   get("/") {
-    session.setMaxInactiveInterval(3600 * 24) // Anti-XSRFに使いたいので JSESSIONIDクッキーを発生させるためだけの操作
+    val cookie = new javax.servlet.http.Cookie("XSRF-TOKEN", session.getId) // セッションIDをXSRFトークンとして使う
+    response.addCookie(cookie)
     Map()
   }
 
   post("/") {
-    // TODO: JSESSIONIDを使ってXSRF対策
+    Option(request.getHeader("X-XSRF-TOKEN")).filter(_.equals(session.getId)).getOrElse(halt(401, """{"info":"Token mismatch"}"""))
     // TODO: プログラムで短時間に大量POSTしようとする厨二病くさい奴を弾く
     val email = new org.apache.commons.mail.SimpleEmail()
     email.setHostName(appConfig.getString("smtp.host"))
     email.setSmtpPort(appConfig.getInt("smtp.port"))
     email.setFrom(appConfig.getString("inquiry.from"))
     email.setSubject(appConfig.getString("inquiry.subject"))
-    email.setMsg(org.json4s.jackson.JsonMethods.pretty(parsedBody))
+    email.setMsg(org.json4s.jackson.JsonMethods.pretty(parsedBody) + s"\nIPアドレス: ${request.getRemoteAddr}")
     email.addTo(appConfig.getString("inquiry.recipient"))
     email.setCharset("ISO-2022-JP")
     email.send
